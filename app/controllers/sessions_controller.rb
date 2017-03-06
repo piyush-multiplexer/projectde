4ec4e787e94890199c8128baa38b394519200278
piyush-multiplexer <thomaspitter10@gmail.com>
2017-03-06 12:02:59 +0530

    Revert "Revert "include session destroy""
    This reverts commit 20046457001f7206cf1b40526b2f55c15279bfdd.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,17 @@ class SessionsController < ApplicationController
     adminuser = Adminuser.find_by(email: params[:session][:email].downcase)
     if adminuser && adminuser.authenticate(params[:session][:password])
       log_in adminuser
-      flash.now[:success] = 'Logged In.. ' + '\n'+ Time.now.to_s
+      flash[:success] = 'Logged In.. '
       redirect_to adminuser
     else
-      flash.now[:danger] = 'Invalid combination!!! ' #+ Time.now.to_s
+      flash.now[:danger] = 'Invalid combination!!! '
       render 'new'
     end
   end
 
   def destroy
-
+    log_out
+    flash[:info] = 'Logged Out...'
+    redirect_to root_url
   end
 end
