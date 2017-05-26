15f3838730cc667557bb3cba4913b159432d9318
Piyush Goyani <thomaspitter10@gmail.com>
2017-05-26 19:18:58 +0530

    Add 2FA

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,89 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303120742) do
+ActiveRecord::Schema.define(version: 20170526121012) do
 
-  create_table "adminusers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
-    t.string   "username"
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "otp_secret_key"
+    t.integer  "otp_module",             default: 0
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "complaints", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "r_fname"
+    t.string   "r_lname"
+    t.string   "r_age"
+    t.string   "r_gender"
+    t.string   "r_email"
+    t.string   "r_phone"
+    t.string   "r_pincode"
+    t.text     "r_address",   limit: 65535
+    t.string   "r_photo"
+    t.string   "v_fname"
+    t.string   "v_lname"
+    t.integer  "v_age"
+    t.string   "v_gender"
+    t.string   "v_phone"
+    t.string   "v_email"
+    t.text     "v_address",   limit: 65535
+    t.string   "v_relation"
+    t.string   "v_photo"
+    t.string   "c_fullname"
+    t.integer  "c_age"
+    t.string   "c_gender"
+    t.text     "c_address",   limit: 65535
+    t.string   "c_photo"
+    t.text     "c_details",   limit: 65535
+    t.string   "cm_type"
+    t.string   "cm_location"
+    t.date     "cm_date"
+    t.string   "cm_rfactor"
+    t.text     "cm_details",  limit: 65535
+    t.string   "cm_photo"
+    t.integer  "user_id"
+    t.string   "complaintid"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "contacts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "name"
+    t.string   "subject"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "otp_secret_key"
+    t.integer  "otp_module",             default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
