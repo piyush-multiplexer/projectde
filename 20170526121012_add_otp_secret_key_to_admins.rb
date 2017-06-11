38af578492674320a1ab87f6c4f525b082d5e25b
Piyush Goyani <thomaspitter10@gmail.com>
2017-06-11 23:50:45 +0530

    Delete 20170526121012_add_otp_secret_key_to_admins.rb

--- 20170526121012_add_otp_secret_key_to_admins.rb
+++
@@ -1,6 +0,0 @@
-class AddOtpSecretKeyToAdmins < ActiveRecord::Migration[5.0]
-  def change
-    add_column :admins, :otp_secret_key, :string
-    add_column :admins, :otp_module, :integer, default: 0
-  end
-end
